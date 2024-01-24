c2dfcf00ba36c08f0162b2adcad61dcb5d0d4e4f
AureoFJunior <aureofajardojunior@hotmail.com>
2024-01-24 11:47:38 -0300

    AB#10 - Adding variables inestead of skipping cause didn't worked.

--- EvangelionERPV2.Web/terraform/main.tf
+++ EvangelionERPV2.Web/terraform/main.tf
@@ -5,39 +5,12 @@ provider "aws" {
 # Create an Amazon ECR repository
 resource "aws_ecr_repository" "evangelionerpv2_repository" {
   name = "evangelionerpv2-repository"
-
-  lifecycle {
-    ignore_changes = [image_scanning_configuration, image_tag_mutability] # Skip and ignore if already exists
-  }
-}
-
-# Create an ECS cluster
-resource "aws_ecs_cluster" "evangelionerpv2_cluster" {
-  name = "evangelionerpv2-cluster"
-}
-
-# IAM role for ECS task execution
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role"
-  
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com",
-        },
-      },
-    ],
-  })
 }
 
 # Attach the AmazonECSTaskExecutionRolePolicy policy to the IAM role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_task_execution_role.name
+  role       = var.aws_ecr_repository_id
 }
 
 # Create an ECS task definition
@@ -48,18 +21,14 @@ resource "aws_ecs_task_definition" "evangelionerpv2_task_definition" {
   cpu                      = "256"
   memory                   = "512"
   
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = var.aws_iam_role_arn
 
   container_definitions = jsonencode([{
     name  = "evangelionerpv2-container"
-    image = "${aws_ecr_repository.evangelionerpv2_repository.repository_url}:latest"
+    image = "${var.aws_ecr_repository_repository_url}:latest"
     cpu   = 256
     memory = 512
   }])
-
-   lifecycle {
-    ignore_changes = [execution_role_arn, container_definitions]  # Add other attributes as needed
-  }
 }
 
 # Create an ECS service
@@ -69,8 +38,4 @@ resource "aws_ecs_service" "evangelionerpv2_service" {
   task_definition = aws_ecs_task_definition.evangelionerpv2_task_definition.arn
   launch_type     = "FARGATE"
   desired_count   = 1
-
-  lifecycle {
-    ignore_changes = [name, cluster, task_definition, launch_type, desired_count]  # Skip and ignore if already exists
-  }
 }
--- EvangelionERPV2.Web/terraform/variables.tf
+++ EvangelionERPV2.Web/terraform/variables.tf
@@ -2,3 +2,17 @@ variable "aws_region" {
 	description = "AWS region"
 	default = "us-east-1"
 }
+
+variable "aws_ecr_repository_id" {
+	description = "AWS ECR repository ID"
+	default = "381492289606"
+}
+variable "aws_ecr_repository_repository_url" {
+	description = "AWS ECR repository URL"
+	default = "381492289606.dkr.ecr.us-east-1.amazonaws.com/evangelionerpv2-repository"
+}
+
+variable "aws_iam_role_arn" {
+	description = "AWS IAM Role ARN"
+	default = "arn:aws:iam::381492289606:role/ecs_task_execution_role"
+}
