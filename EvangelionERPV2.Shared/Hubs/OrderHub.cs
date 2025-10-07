4c5a52ef10e8e739e601680ba71683cd7e9a553b
AureoFJunior <aureofajardojunior@hotmail.com>
2025-10-06 21:20:59 -0300

    AB#21 - Reports for Stock and Order

--- EvangelionERPV2.Shared/Hubs/OrderHub.cs
+++ EvangelionERPV2.Shared/Hubs/OrderHub.cs
@@ -4,5 +4,9 @@ namespace EvangelionERPV2.Shared.Hubs
 {
     public class OrderHub : Hub
     {
+        public async Task SendOrderUpdate(string orderId, string status)
+        {
+            await Clients.All.SendAsync("ReceiveOrderUpdate", orderId, status);
+        }
     }
 }
